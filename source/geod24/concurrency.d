8b63971cb757d25b480711292e46b832eb928fcf
Geod24 <pro.mathias.lang@gmail.com>
2021-03-16 14:25:21 +0900

    Select: Simplify and optimize iteration
    Instead of creating a new GC-allocated array, iterating on it,
    and doing one or two pointer comparison each iteration,
    just break the code into two iterations.

--- source/geod24/concurrency.d
+++ source/geod24/concurrency.d
@@ -736,18 +736,18 @@ public SelectReturn select (ref SelectEntry[] read_list, ref SelectEntry[] write
     read_list = read_list.randomShuffle();
     write_list = write_list.randomShuffle();
 
-    foreach (ref list; [read_list, write_list])
+    foreach (ref entry; read_list)
     {
-        foreach (ref entry; list)
-        {
-            if (ss.isConsumed())
-                break;
+        if (ss.isConsumed())
+            break;
+        entry.selectable.selectRead(entry.select_data, ss, sel_id++);
+    }
 
-            if (list is read_list)
-                entry.selectable.selectRead(entry.select_data, ss, sel_id++);
-            else if (list is write_list)
-                entry.selectable.selectWrite(entry.select_data, ss, sel_id++);
-        }
+    foreach (ref entry; write_list)
+    {
+        if (ss.isConsumed())
+            break;
+        entry.selectable.selectWrite(entry.select_data, ss, sel_id++);
     }
 
     if (!ss.blocker.wait(timeout))
